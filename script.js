1e07d1a4a03298d024a91d11a21a40f6ceaf413d
Sam <sam0000@live.nl>
2020-12-14 13:47:52 +0100

    reviews almost done

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+/* reviews */
+$(document).ready(function () {
+	$('.testiSlide').slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1500,
+		responsive: [{
+			breakpoint: 850,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		}]
+	});
+});
